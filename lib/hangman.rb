18f8cc1ca3b87a11dcdaf54894fadc5580d13f36
murgraydj <murgraydj@gmail.com>
2023-02-21 13:48:55 -0500

    Picks word for user to guess

--- lib/hangman.rb
+++ lib/hangman.rb
@@ -1,15 +1,24 @@
+#Author: Murgray D. John
+#Date created: 2/21/2023
+#Purpose: To play a simple game of hangman against a computer
 
-dictionary = []
 
-def create_dictionary(dictionary)
+def create_dictionary
+  dictionary = []
   all_words = File.readlines('google-10000-english-no-swears.txt')
   all_words.each do |word|
     if word.length > 5 && word.length < 12
       dictionary << word
     end
   end
+  dictionary
 end
 
-create_dictionary(dictionary)
+def new_game
+  dictionary = create_dictionary
+  word_to_guess = dictionary.sample
+  puts word_to_guess
+end
 
-puts dictionary
+new_game
+
