7042d3d6c354dabf763785973050cd0e2cb88b33
murgraydj <murgraydj@gmail.com>
2023-03-01 17:04:07 -0500

    Fixes loading of game_info.rb into hangman.rb
    There was an error due to the use of 'require.' This was fixed by
    changing 'require' to 'require_relative.' This fixed also changed the
    class of game_info from 'Board' to 'GameInfo.'

--- lib/game_info.rb
+++ lib/game_info.rb
@@ -1,10 +1,10 @@
-class Board
+class GameInfo
   attr_accessor :guesses
   attr_accessor :errors
   attr_accessor :word_to_guess
 
   def initialize(word_to_guess)
-    @guesses = 0
+    @guesses = ""
     @errors = 0
     @word_to_guess = word_to_guess
   end
--- lib/hangman.rb
+++ lib/hangman.rb
@@ -1,81 +1,89 @@
 #Author: Murgray D. John
 #Date created: 2/21/2023
 #Purpose: To play a simple game of hangman against a computer
+require_relative 'game_info.rb'
 
+class Hangman
 
-def create_dictionary
-  dictionary = []
-  all_words = File.readlines('google-10000-english-no-swears.txt')
-  all_words.each do |word|
-    if word.length > 5 && word.length < 12
-      dictionary << word
-    end
+  def initialize
+    welcome_player
   end
-  dictionary
-end
 
-# def play_round
-#   while()
-# end
-
-def show_rules
-  puts "How to play: "
-  puts "You have to correctly guess all of the letters in the word."
-  puts "If you make 7 mistakes, you lose!"
-  puts "Let's get started!"
-end
-
-def new_game
-  dictionary = create_dictionary
-  word_to_guess = dictionary.sample
-  puts "New game started!"
-  show_rules
-end
-
-def saved_or_new_game(choice)
-  puts
-  if choice == "1"
-    new_game
-  else
-    puts "This functionality has not been implemented yet!"
-  end
-end
-
-def get_valid_data(prompt, response, valid_responses) 
-  if response.nil?
-    print prompt
-    response = gets.chomp
-  else
-    valid_responses.each do |valid_response|
-      if response.downcase == valid_response.downcase
-        return response
-      elsif response.downcase == "exit"
-        puts "Thank you for playing!"
-        exit!
-      elsif response.downcase == "help"
-        print_actions(get_file_contents)
-        break
+  def create_dictionary
+    dictionary = []
+    all_words = File.readlines('google-10000-english-no-swears.txt')
+    all_words.each do |word|
+      if word.length > 5 && word.length < 12
+        dictionary << word
       end
     end
-    response = nil
+    dictionary
   end
-  response = get_valid_data(prompt, response, valid_responses)  
+
+  # def play_round
+  #   while()
+  # end
+
+  def show_rules
+    puts "How to play: "
+    puts "You have to correctly guess all of the letters in the word."
+    puts "If you make 7 mistakes, you lose!"
+    puts "Let's get started!"
+  end
+
+  def new_game
+    dictionary = create_dictionary
+    word_to_guess = dictionary.sample
+    game_file = GameInfo.new(word_to_guess)
+    puts "New game started!"
+    show_rules
+  end
+
+  def saved_or_new_game(choice)
+    puts
+    if choice == "1"
+      new_game
+    else
+      puts "This functionality has not been implemented yet!"
+    end
+  end
+
+  def get_valid_data(prompt, response, valid_responses) 
+    if response.nil?
+      print prompt
+      response = gets.chomp
+    else
+      valid_responses.each do |valid_response|
+        if response.downcase == valid_response.downcase
+          return response
+        elsif response.downcase == "exit"
+          puts "Thank you for playing!"
+          exit!
+        elsif response.downcase == "help"
+          print_actions(get_file_contents)
+          break
+        end
+      end
+      response = nil
+    end
+    response = get_valid_data(prompt, response, valid_responses)  
+  end
+
+  def welcome_player
+    puts "Hi and welcome to Hangman!"
+    print "What's your name?: "
+    player_name = gets.chomp
+    puts "\nHi #{player_name}!"
+    puts "Would you like to: "
+    puts "   1. Start a new game"
+    puts "            OR"
+    puts "   2. Open a saved game"
+    action_prompt = "Please enter 1 or 2 for an action: "
+    action_choices = %w(1 2)
+    choice = get_valid_data(action_prompt, nil, action_choices)
+    saved_or_new_game(choice)
+  end
 end
 
-def welcome_player
-  puts "Hi and welcome to Hangman!"
-  print "What's your name?: "
-  player_name = gets.chomp
-  puts "\nHi #{player_name}!"
-  puts "Would you like to: "
-  puts "   1. Start a new game"
-  puts "            OR"
-  puts "   2. Open a saved game"
-  action_prompt = "Please enter 1 or 2 for an action: "
-  action_choices = %w(1 2)
-  choice = get_valid_data(action_prompt, nil, action_choices)
-  saved_or_new_game(choice)
-end
-
-welcome_player
+Hangman.new
 
