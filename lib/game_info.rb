ecc81b3ad53e60ae678d40a61569dacfd65839e7
murgraydj <murgraydj@gmail.com>
2023-03-02 14:07:48 -0500

    Generates placeholder for users guesses
    Each letter in the word to guess is represented by a "_"

--- lib/game_info.rb
+++ lib/game_info.rb
@@ -2,10 +2,12 @@ class GameInfo
   attr_accessor :guesses
   attr_accessor :errors
   attr_accessor :word_to_guess
+  attr_accessor :guess_so_far
 
   def initialize(word_to_guess)
     @guesses = ""
     @errors = 0
     @word_to_guess = word_to_guess
+    @guess_so_far = Array.new(word_to_guess.length) {"_"}
   end
 end
