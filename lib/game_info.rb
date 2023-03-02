4e8eb152f06e68aff18c9f3bad1918ae17dd6856
murgraydj <murgraydj@gmail.com>
2023-03-02 14:26:45 -0500

    Corrects length of word placeholder
    Before, it was the length of the word + 1 extra space. Now it is the
    length of the word.

--- lib/game_info.rb
+++ lib/game_info.rb
@@ -8,6 +8,6 @@ class GameInfo
     @guesses = ""
     @errors = 0
     @word_to_guess = word_to_guess
-    @guess_so_far = Array.new(word_to_guess.length) {"_"}
+    @guess_so_far = Array.new(word_to_guess.length - 1) {"_"}
   end
 end
